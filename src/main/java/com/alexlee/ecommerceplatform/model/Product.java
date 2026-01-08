b4f306c2155fcd31634a7d58384cdf1342ba4a2b
Alex <alexlee94@gmail.com>
2026-01-08 18:12:43 -0500

    feat: Implement Product entity model

---
+++ src/main/java/com/alexlee/ecommerceplatform/model/Product.java
@@ -0,0 +1,22 @@
+package com.alexlee.ecommerceplatform.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String brand;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String imageUrl;
+
+}
